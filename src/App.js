2b0b4187a3ad29e868efe3001fddbe18666b8301
Divyanshu Rana <ranadivyanshu25@gmail.com>
2022-08-05 09:39:00 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.js";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
